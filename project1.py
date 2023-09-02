1108b714ce8486867b975ce6fbb1fb1515cf2e15
LeaBani <113905453+LeaBani@users.noreply.github.com>
2023-09-02 14:06:10 +0200

    update scores

--- project1.py
+++ project1.py
@@ -22,10 +22,36 @@ while True:
 
     else:
         print("Invalid try again")
-        
+
 # print(players)
 
 max_score = 50
 player_scores = [0 for _ in range(players)] # pour chaque joueur, on place un "0" dans la liste des scores / range permet de boucler
 
-print(player_scores)
+# print(player_scores)
+
+while max(player_scores) < max_score:
+
+    current_score = 0 # initialisation de la variable
+
+    for player_index in range(players):
+        print("\nPlayer number", player_index + 1, "turn has started!\n") # \n ajoute un saut à la ligne
+        while True:
+            should_roll = input("Would you like to roll (y)?")
+            if should_roll.lower() != "y": # on parse 
+                break
+
+            value = roll()
+            if value == 1:
+                current_score = 0
+                print("You rolled a 1! Turn done!")
+            else:
+                current_score += value # incrémente
+                print("You rolled a: ", value)
+
+            print("Your score is:", current_score)
+
+        player_scores[player_index] += current_score # j'incremente le score du joueur
+        print("Your total score is:", player_scores[player_index]) # j'affiche le score du joueur
+
+
